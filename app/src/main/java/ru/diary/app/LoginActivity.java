a390889fe7c543ceaf25ee345d61c9d9455f109e
Darya Schetinina <mooneuq@gmail.com>
2015-04-01 23:55:29 +0600

    Add Login activity

---
+++ app/src/main/java/ru/diary/app/LoginActivity.java
@@ -0,0 +1,18 @@
+package ru.diary.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by Daria Schetinina on 4/1/15.
+ */
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+}
