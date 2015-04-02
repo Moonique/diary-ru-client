081f5eb7f6997e297b8d08f65608db78851bb4cc
Darya Schetinina <mooneuq@gmail.com>
2015-04-02 23:10:53 +0600

    Created second activity Favorites

---
+++ app/src/main/java/ru/diary/app/FavoritesActivity.java
@@ -0,0 +1,16 @@
+package ru.diary.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Daria Schetinina on 4/2/15.
+ */
+public class FavoritesActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_favorites);
+	}
+}
--- app/src/main/java/ru/diary/app/LoginActivity.java
+++ app/src/main/java/ru/diary/app/LoginActivity.java
@@ -1,7 +1,11 @@
 package ru.diary.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -9,11 +13,32 @@ import android.os.Bundle;
  */
 public class LoginActivity extends Activity {
 
+	private EditText loginEditText;
+	private EditText passwordEditText;
+	private Button loginButton;
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		getActionBar().setTitle(R.string.app_name);
+		findViews();
+		setListeners();
+	}
+
+	private void setListeners() {
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getBaseContext(), FavoritesActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+	private void findViews() {
+		loginEditText = (EditText) findViewById(R.id.activity_login_login_edit_text);
+		passwordEditText = (EditText) findViewById(R.id.activity_login_password_edit_text);
+		loginButton = (Button) findViewById(R.id.activity_login_login_button);
 	}
 
 }
